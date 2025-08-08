c2564f070054453d04ce1e767ee0cfcb7b17bc07
druchniewicz <druchniewicz@soldevelo.com>
2025-08-09 00:03:11 +0200

    Changes for Vxnaid INRB

--- cfl/startLocalDistro.sh
+++ cfl/startLocalDistro.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export COMPOSE_PROJECT_NAME=inrb
-docker compose -f docker-compose.run.yml -f docker-compose.db.yml up -d --force-recreate
+docker compose -f docker-compose.run.yml -f docker-compose.db.yml up -d --build
--- cfl/startProdDistro.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export COMPOSE_PROJECT_NAME=inrb
-docker compose -f docker-compose.run.yml up -d --force-recreate
